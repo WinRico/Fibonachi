612f0fb72961307af4ecdc3add84cffb10ed4f86
WinRico <yaroslamud660@example.com>
2023-11-05 22:11:00 +0200

    Initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,51 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class MainTest {
+
+    @Test
+    public void testFibonacci() {
+        assertEquals(0, Fibonacci.fib(0));
+        assertEquals(1, Fibonacci.fib(1));
+        assertEquals(1, Fibonacci.fib(2));
+        assertEquals(2, Fibonacci.fib(3));
+        assertEquals(3, Fibonacci.fib(4));
+        assertEquals(5, Fibonacci.fib(5));
+        assertEquals(8, Fibonacci.fib(6));
+        assertEquals(13, Fibonacci.fib(7));
+        assertEquals(21, Fibonacci.fib(8));
+        assertEquals(34, Fibonacci.fib(9));
+    }
+    @Test
+    public void testFibonacciWithZero() {
+        int result = Fibonacci.fib(0);
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public  void testFibonacciWithPositiveNumber() {
+        int result = Fibonacci.fib(5);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void testFibonacciWithNegativeNumber() {
+        int result = Fibonacci.fib(-3);
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void testFibonacciWithLargeNumber() {
+        int result = Fibonacci.fib(10);
+        Assertions.assertEquals(55, result);
+    }
+
+    @Test
+    public void testFibonacciWithOne() {
+        int result = Fibonacci.fib(1);
+        Assertions.assertEquals(1, result);
+    }
+}
